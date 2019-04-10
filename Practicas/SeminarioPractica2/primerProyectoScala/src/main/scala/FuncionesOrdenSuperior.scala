415f2607c4119eed11ed6f40292d02f9deb26d48
Antonio <Ancaor@correo.ugr.es>
2019-04-10 11:36:37 +0200

    finClase

--- Practicas/SeminarioPractica2/primerProyectoScala/src/main/scala/FuncionesOrdenSuperior.scala
+++ Practicas/SeminarioPractica2/primerProyectoScala/src/main/scala/FuncionesOrdenSuperior.scala
@@ -8,4 +8,55 @@ object BuscadorArchivos {
     for(archivo<-archivos
       if(archivo.getName.endsWith(extension))) yield archivo
   }
+
+  def archivosConCadena(cadena : String) = {
+    for(archivo<-archivos
+        if(archivo.getName.contains(cadena))) yield archivo
+  }
+
+  def archivoConExpresionRegular(expresion:String) = {
+    for(archivo<-archivos
+        if(archivo.getName.matches(expresion))) yield archivo
+  }
+
+
+}
+
+
+object BuscadorArchivosSimple {
+  private def archivos = (new File(".")).listFiles()
+
+  //Patron estrategia
+  def archivosCondicion(cadena:String, condicion:(String,String) => Boolean) = {
+    for(archivo<- archivos
+      if(condicion(archivo.getName,cadena))) yield archivo
+  }
+
+  def archivosConExtension(extension:String) = {
+    archivosCondicion(extension,_.endsWith(_))
+    // archivosCondicion(extension, (cadena1,cadena2) => cadena1.endsWith(cadena2)) // equivalente a la de arriba
+  }
+
+  def archivosConCadena(cadena:String) = {
+    archivosCondicion(cadena,_.contains(_))
+  }
+
+  def archivosConExpresion(expresion:String) = {
+    archivosCondicion(expresion,_.matches(_))
+  }
+}
+
+object BuscadorArchivosMasSimple{
+  private def archivos = (new File(".")).listFiles()
+
+  def archivosCondicion(condicion: String => Boolean) = {
+    for(archivo <- archivos
+      if(condicion(archivo.getName))) yield archivo
+  }
+
+  def archivosConExtension(extension:String) = {
+    archivosCondicion(_.endsWith(extension))
+    //archivosCondicion(cadena => cadena.endsWith(extension))
+  }
+
 }
--- Practicas/SeminarioPractica2/primerProyectoScala/src/main/scala/OrdenacionGenerica.scala
+++ Practicas/SeminarioPractica2/primerProyectoScala/src/main/scala/OrdenacionGenerica.scala
@@ -1,3 +1,38 @@
+import scala.annotation.tailrec
+
 object OrdenacionGenerica {
+  def ordenado[A](coleccion : Array[A], criterio: (A,A) => Boolean) : Boolean = {
+    @tailrec
+    def go(indice : Int) : Boolean = {
+      //primer caso base
+      if(criterio(coleccion(indice),coleccion(indice+1)) == false) false
+      else{
+        //Segundo caso base
+        if(indice == coleccion.length - 2) criterio(coleccion(indice),coleccion(indice+1))
+        else(go(indice+1))
+      }
+    }
+    go(0)
+  }
+  def ordenadoC[A](array:Array[A])(criterio: (A,A) => Boolean) : Boolean = {
+    @tailrec
+    def go(indice : Int) : Boolean = {
+      //primer caso base
+      if(criterio(array(indice),array(indice+1)) == false) false
+      else{
+        //Segundo caso base
+        if(indice == array.length - 2) criterio(array(indice),array(indice+1))
+        else(go(indice+1))
+      }
+    }
+    go(0)
+  }
+
+  val array1 : Array[Int] = Array(1,5,6,7)
+  OrdenacionGenerica.ordenado(array1,(x:Int,y:Int) => x>y)
+
+  OrdenacionGenerica.ordenadoC(array1)(_>_)
+
+  val res3: Array[A] => Boolean = OrdenacionGenerica.ordenadoC(_)((x:Int, y:Int) => x > y)
 
 }
